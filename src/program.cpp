6c2a78c9554813d458e850f76362b00ff67e54a5
Caio Cezar <127907211+ccezar25@users.noreply.github.com>
2023-12-23 04:52:56 -0300

    Add files via upload

---
+++ src/program.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std;
+    
+    int main (){
+
+cout << "\n";
+cout << "*********************************************" << endl;        
+cout << "*     Bem-vindos ao jogo da adivinhacao!    *" << endl;
+cout << "*********************************************" << endl;
+
+cout << "\n";
+
+
+
+
+return 0;
+}
